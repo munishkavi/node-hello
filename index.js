0924ef0039d8bb85a99bb90a6b843dbecd87ec83
Munishkumar R <149291053+munishkavi@users.noreply.github.com>
2025-11-10 13:02:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'nothing change if nothing changes...\n'
+  const msg = 'bye bye bye\n'
   res.end(msg);
 });
 
